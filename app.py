ec21d962e5fe0c06c9f5e247fae29bb6eceb175f
SencerH <sencerhamarat@gmail.com>
2020-01-30 23:57:36 +0300

    Fetching city method with json response added

--- app.py
+++ app.py
@@ -61,9 +61,32 @@ def upload():
     })
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/fetch/<country_code>/')
+def fetch_city(country_code=None):
+    success = False
+    rows = []
+    if country_code is None or not country_code:
+        return jsonify({
+            'status': success,
+            'data': rows,
+            'message': 'You have to specify country code; Eg. http://example.com/fetch/us/'
+        })
+
+    with sqlite3.connect(DB_NAME) as con:
+        cur = con.cursor()
+        cur.execute(
+            "SELECT city_name FROM '3piano_cities' WHERE country_code=?", (country_code.lower(), ))
+        rows = cur.fetchall()
+
+    data = []
+    for row in rows:
+        data.append(row[0])
+    success = True
+    return jsonify({
+            'status': success,
+            'data': data,
+            'message': ''
+        })
 
 
 if __name__ == '__main__':
